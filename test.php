4a69b49c22b7e0465fbe1adeeaa7d6c623f002f5
DaemonDeathAngel <PhantomPCBusiness@gmail.com>
2020-07-09 22:18:20 -0400

    Initial commit

---
+++ test.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Phantom PC - Custom PCs to Fit Your Budget</title>
+        <meta name="description" content="">
+		<link rel="stylesheet" type="text/css" href="test.css" />
+
+    </head>
+    <body>
+		
+		<div class="PageCont">
+		
+			<div class="Header">
+			
+				<div class="LogoBox">
+					<img class="Logo" src="images/PhantomPCText.png">
+				</div>
+				
+				<div class="NavBox">
+					<a class="NavLink" href="index.php">Home</a>
+				</div>
+				
+				<div class="NavBox">
+					<a class="NavLink" href="pricing.php">Pricing</a>
+				</div>
+				
+				<div class="NavBox">
+					<a class="NavLink" href="#">Reviews</a>
+				</div>
+				
+				<div class="NavBox">
+					<a class="NavLink" href="#">Contact</a>
+				</div>
+				
+				<div class="NavBox">
+					<a class="NavLink" href="about.php">About Us</a>
+				</div>
+				
+			</div>
+			
+			<div class="Content">
+				Content
+			</div>
+			
+			<div class="Footer">
+				Footer
+			</div>
+			
+		</div>
+		
+    </body>
+</html>
